ea1e2092fa9b97f47c81683bf3497f84916e7158
Makadoro <tim.koehler86@googlemail.com>
2023-11-14 15:06:31 +0100

    Added server api

--- app/chat_minimal.py
+++ app/chat_minimal.py
@@ -1,12 +1,13 @@
 import gradio as gr
 import json
 from openai import OpenAI
+import os
+from dotenv import load_dotenv
 
-# Get openai key from config
-with open("key.json") as f:
-    data = json.load(f)
-    api_key = data["openAiKey"]
-
+# Load .env file
+load_dotenv()
+# Get openai key from environment variable
+api_key = os.getenv("OPEN_AI_KEY")
 client = OpenAI(api_key=api_key)
 
 
@@ -34,7 +35,7 @@ demo = gr.ChatInterface(
     ),
     additional_inputs=[
         gr.Audio(sources=["microphone"], label="Speak", type="filepath"),
-        #gr.ClearButton("audio_input"),
+        gr.ClearButton("audio_input"),
     ],
     submit_btn="Abschicken",
     retry_btn=None,
@@ -42,7 +43,7 @@ demo = gr.ChatInterface(
     clear_btn=None,
     chatbot=gr.Chatbot(
         avatar_images=[
-            "https://files.oaiusercontent.com/file-8lKLS02Q7fc70GD4HGT3Fwgp?se=2023-11-12T12%3A37%3A49Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3D2c898f81-e907-4adc-8625-235b271f82a1.webp&sig=AuVp72MSzgqmJRqsxZI%2BmCtSOBQqI2GCrRkbv8fAjEs%3D",
+            "/app/human.png",
             "https://files.oaiusercontent.com/file-1tU7eUavgU9wYb5aRVNpAq3v?se=2023-11-12T12%3A39%3A13Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3D7bb2cd2c-87b9-483f-a3df-7b374962baf7.webp&sig=JZRFKtrb79tO7CQQEd50Z/VMPXl7HokRi1Tm93oei94%3D",
         ]
     )
